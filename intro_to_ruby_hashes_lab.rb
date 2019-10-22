eaff4617a2d430d901ebe770fdfd1ff9197a0436
Randy Steele <steelerx155@gmail.com>
2019-10-22 00:25:40 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,24 +1,31 @@
 def new_hash
-
+new_hash = {}
+new_hash
 end
 
 def actor
-
+actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {:railroads =>{} }
 
 end
 
 def monopoly_with_second_tier
+  {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {} 
+  }
+  }
+  
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
 def monopoly_with_third_tier
+   {:railroads => {:pieces => 4, :names =>  {:rent_in_dollars => {}  }
+    }
+    }
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
 end
 
 def monopoly_with_fourth_tier
